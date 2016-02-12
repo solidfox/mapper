36243ad4b93ff9dd8681d37f62de5ab34c591a1b
Daniel Schlaug <daniel.schlaug@gmail.com>
2016-02-12 19:31:03 +0100

    Added support for variations of the ISO 8601

--- Sources/NSDate+Convertible.swift
+++ Sources/NSDate+Convertible.swift
@@ -6,15 +6,39 @@ import Foundation
  This implementation assumes the given is a string containing a date on the
  ISO 8601 compliant form produced by JavaScript's toJSON method.
  E.g. 2012-04-23T18:25:43.511Z
- if not a `MapperError` is thrown
+ 
+ It also supports variations such as
+ 2012-04-23T18:25:43Z
+ 2012-04-23T18:25:43GMT+01
+ 2012-04-23Z
+ 
+ Note that some form of time zone indication is required to avoid localization ambiguity.
+ 
+ For unsupported formats a `MapperError` is thrown
  */
 extension NSDate:Convertible {
     public static func fromMap(value: AnyObject?) throws -> NSDate {
+        guard let string = value as? String else {
+            throw MapperError()
+        }
+        
+        let supportedDateFormats = [
+            "yyyy-MM-dd'T'HH:mm:ss.SSSz",
+            "yyyy-MM-dd'T'HH:mm:ssz",
+            "yyyy-MM-ddz",
+            "yyyyMMdd'T'HHmmss.SSSz",
+            "yyyyMMdd'T'HHmmssz",
+            "yyyyMMddz"
+        ]
+        
         let dateFormatter = NSDateFormatter()
         dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSz"
-        if let string = value as? String, let date = dateFormatter.dateFromString(string) {
-            return date
+        
+        for dateFormat in supportedDateFormats {
+            dateFormatter.dateFormat = dateFormat
+            if let date = dateFormatter.dateFromString(string) {
+                return date
+            }
         }
         
         throw MapperError()
